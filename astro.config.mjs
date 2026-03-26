966f64d25714519b03c381f18feb63d67fcaf52a
Al-Tareen <altareenk@outlook.com>
2026-03-25 20:39:02 -0500

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   output: "server",
+  site: "https://altareen.com",
+  integrations: [sitemap()],
   adapter: node({
     mode: "standalone",
+    
   }),
 });
